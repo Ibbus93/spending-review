290aaf706d85b10c5767f9bc63cfc3cdc0cfd5e7
Federico <federico.ibba@tui.com>
2024-06-23 17:23:15 +0200

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <UContainer class="mt-10">
+    <UCard>
+      <template #header>
+        <div class="flex items-center">
+          <span class="i-heroicons-banknotes w-8" />
+          <span>Spending review</span>
+        </div>
+      </template>
+
+      <template #body> </template>
+
+      <Placeholder class="h-32" />
+
+      <template #footer>
+        <Placeholder class="h-8" />
+      </template>
+    </UCard>
+  </UContainer>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  future: {
+    compatibilityVersion: 4,
+  },
+  css: ['~/assets/main.scss'],
+})
